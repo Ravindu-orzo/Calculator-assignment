f21cbbf8361400f7880c7e95fb26c9c3f481ea1e
Ravindu-orzo <ravindu.m.wijayarathna@gmail.com>
2023-06-08 07:44:49 +0530

    added numpadLock to prevent user concatenating numbers to the result
    one major issue remains: user can add as many decimal points (.) they want. need to limit it to one decimal point. Also need to add a 0 when decimal point pressed as the first input

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-let record = ""; //remembers user input
+let displayNum = ""; //remembers user input
 let operatorLock = true; //toggles operator panel ON and OFF
-
+let numpadLock = false; //toggles numpad ON and OFF
 
 //Displays user input numbers
 function recorder(input){ 
-  record += input;
-  document.getElementById('display').value += input;
-  operatorLock = false;
+  if(numpadLock == false){
+    displayNum += input;
+    document.getElementById('display').value = displayNum;
+    
+    operatorLock = false; 
+  } else{
+    //do nothing
+  }
 }
 
 //Displays user input operators, prevent stacking multiple operators
 function operator(input){
   if(operatorLock == false){
-    record += input;
-    document.getElementById('display').value += input;
+    displayNum += input;
+    document.getElementById('display').value = displayNum;
+    
     operatorLock = true;
+    numpadLock = false;
   } else {
     //do nothing
   }
@@ -23,18 +30,35 @@ function operator(input){
 
 //Displays result
 function evaluator(){
-  document.getElementById('display').value = eval(record);
-  record = eval(record);
+  document.getElementById('display').value = eval(displayNum);
+  let result = eval(displayNum);
+  displayNum = result.toString();
+  
+  numpadLock = true;
+  operatorLock = false;
 }
 
 //Resets display & memory
 function reset(){ 
-  record = "";
-  document.getElementById('display').value = record;
+  displayNum = "";
+  document.getElementById('display').value = displayNum;
+  
+  numpadLock = false;
+  operatorLock = true;
 }
 
 
-/////////DEV NOTES ////////
-//the operator lock is 'true' by default. This prevents an user entering an operator first before a digit. 
-//(functionality for entering negetive values is not avaialble because of this. This will be fixed later)
+
+/////////DEV NOTES --- HOW NUMPAD LOCK AND OPERATOR PANEL LOCK WORKS ////////
+//the operator lock is 'true' by default. This prevents a user from entering an operator first before a number. 
+//(however, functionality for entering negetive values is not avaialble because of this.)
+
+//the numpad lock is 'false' by default. But numpad locks after hitting equal sign. this prevents the user from adding numbers 
+//to the result (ex: if result was 67 and if user hit the 8 key, 8 will concatenate to the result like this 678. numpad lock prevents this)
+
+//after getting the result, user can enter an operator to unlock the numpad and then can type in a number. this will allow the 
+//user to perform calculations on the result
+
+
+//made by Ravindu Wijayarathna--> for evotech web dev course//
 
